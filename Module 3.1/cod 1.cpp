9d2968fde75b896b820542f7110575a3b374418c
NirmalPtl <140416166+NirmalPtl@users.noreply.github.com>
2023-12-22 14:54:04 +0530

    Delete Module 3.1 directory

--- Module 3.1/cod 1.cpp
+++
@@ -1,9 +0,0 @@
-//write A Proramme to print hello world
-#include<stdio.h>
-int main()
-{
-	printf("Nirmal Sankhavra\n");
-	printf("28 January 2003\n");
-	printf("21 year\n");
-	printf("A-32, Shandar Recidensy, old Morbi road, Rajkot");
-}
